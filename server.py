4756ad20cb93aac1860680ca7ce22837407dc207
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-11-05 15:43:22 +0300

    Added caching of info about top rated movies

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from flask_caching import Cache
+
+from cinemas import get_top_rated_movies_info
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': 'cache'})
+
+
+@cache.cached(timeout=3600, key_prefix='movies')
+def get_movies_info(movies_count=10):
+    return get_top_rated_movies_info(movies_count)
+
 
 @app.route('/')
 def films_list():
-    return render_template('films_list.html')
+    movies_info = get_movies_info()
+
+    return render_template('films_list.html', movies_info=movies_info)
+
 
 if __name__ == "__main__":
     app.run()
